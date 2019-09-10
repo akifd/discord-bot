78e35182618450f3526af35aceacc15124a4c67a
Aki Foudila <aki.foudila@fns.fi>
2019-09-10 21:13:23 +0300

    Add the add command functionality

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -10,14 +11,28 @@ token = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
+def create_connection():
+    return sqlite3.connect("sqlite.db")
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
 @bot.command(name="add")
-async def add(context):
-    await context.send("Add functionality coming soon!")
+async def add(context, timestamp):
+    connection = create_connection()
+
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO event (timestamp) VALUES (?)", (timestamp,))
+        event_id = cursor.lastrowid
+
+    message = f"Added event #{event_id} on {timestamp}."
+
+    print(message)
+    await context.send(message)
 
 
 @bot.command(name="list")
